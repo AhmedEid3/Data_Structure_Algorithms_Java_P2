11e244e36b66d6f587cfba96837b63e228c71825
AhmedEid3 <ahmed.eid3@outlook.com>
2023-03-21 19:16:46 +0200

    Implement Heap DS

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,35 @@
-import ds.AVLTree;
+import ds.Heap;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        var tree = new AVLTree();
-
-        tree.insert(10);
-        tree.insert(30);
-        tree.insert(20);
-//        tree.insert(5);
-//        tree.insert(3);
-//        tree.insert(12);
-//        tree.insert(10);
+        var tree = new Heap(3);
+//
 //        tree.insert(15);
-//        tree.insert(14);
+//        tree.insert(10);
+//        tree.insert(3);
+//        tree.insert(8);
+//        tree.insert(12);
+//        tree.insert(9);
+//        tree.insert(4);
+//        tree.insert(1);
+//        tree.insert(24);
+//
+//        System.out.println(tree);
+//        System.out.println(tree.remove());
+//        System.out.println(tree);
 
+        int[] numbers = {5, 1, 3, 9, 6, 10};
+        for (var number: numbers
+             ) {
+            tree.insert(number);
+        }
 
-        System.out.println(tree);
+        for (int i = numbers.length - 1; i >=0 ; i--) {
+            numbers[i] = tree.remove();
+        }
+
+        System.out.println(Arrays.toString(numbers));
     }
 }
---
+++ src/ds/Heap.java
@@ -0,0 +1,124 @@
+package ds;
+
+public class Heap {
+    private int[] items;
+    private int size = 0;
+
+    public Heap(int length) {
+        items = new int[length];
+    }
+
+    public void insert(int item) {
+        if (isFull()) resize();
+        items[size++] = item;
+        bubbleUp();
+    }
+
+    public int remove() {
+        if (isEmpty()) throw new IllegalStateException("Heap is empty");
+
+        int removedValue = items[0];
+        items[0] = items[--size];
+        bubbleDown();
+
+        return removedValue;
+    }
+
+    private void bubbleDown() {
+        int index = 0;
+        while (!isParentValid(index)) {
+            swap(index, largeChildIndex(index));
+            index = leftIndex(index);
+        }
+    }
+
+
+    private void bubbleUp() {
+        int index = size - 1;
+
+        while (index > 0 && items[index] > items[parentIndex(index)]) {
+            swap(index, parentIndex(index));
+            index = parentIndex(index);
+        }
+    }
+
+    private void swap(int firstIndex, int secondIndex) {
+        int temp = items[firstIndex];
+        items[firstIndex] = items[secondIndex];
+        items[secondIndex] = temp;
+    }
+
+
+    private int parentIndex(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int leftIndex(int index) {
+        return index * 2 + 1;
+    }
+
+    private int rightIndex(int index) {
+        return index * 2 + 2;
+    }
+
+    private int largeChildIndex(int index) {
+        if (!hasLeftChild(index)) return index;
+
+        if (!hasRightChild(index)) return leftIndex(index);
+
+        return leftChild(index) > rightChild(index) ? leftIndex(index) : rightIndex(index);
+    }
+
+    private boolean isParentValid(int index) {
+        if (!hasLeftChild(index)) return true;
+
+        var isValid = items[index] >= leftChild(index);
+        if (hasRightChild(index))
+            isValid &=
+                    items[index] >= rightChild(index);
+
+        return isValid;
+    }
+
+    private boolean hasLeftChild(int index) {
+        return leftIndex(index) < size;
+    }
+
+    private boolean hasRightChild(int index) {
+        return rightIndex(index) < size;
+    }
+
+    private int rightChild(int index) {
+        return items[rightIndex(index)];
+    }
+
+    private int leftChild(int index) {
+        return items[leftIndex(index)];
+    }
+
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    private boolean isFull() {
+        return size == items.length;
+    }
+
+    private void resize() {
+        var newArray = new int[size * 2];
+        for (int i = 0; i < size; i++) newArray[i] = items[i];
+        items = newArray;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer out = new StringBuffer("[");
+        for (int i = 0; i < size; i++) {
+            out.append(items[i]);
+            if (i < size - 1) out.append(", ");
+        }
+        return out.append("]").toString();
+    }
+}
